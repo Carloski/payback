0a6964303a6452058a55ca179da7754da63036f5
Dvlprdad <carlosmtamayo@gmail.com>
2021-09-15 18:46:57 -0400

    Updated App.index

--- js/site.js
+++ js/site.js
@@ -1,4 +1,24 @@
-//Testing Prism
-function helloWorld(){
-    alert('Hello World!');
+//Get values from user
+function getValues() {
+    let loan = document.getElementById('loanAmount').value;
+    let payments = document.getElementById('payments').value;
+    let rate = document.getElementById('rate').value;
+
+    parse the values
+
+    //Logic Function
+
+    //Display Results
+}
+
+
+
+//Logic function
+
+
+
+//Display results
+function displayResults() {
+    let tableBody = document.getElementById('results');
+    let templateRow = document.getElementById('pbtemplate');
 }
